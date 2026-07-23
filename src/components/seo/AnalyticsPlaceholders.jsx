24abef8dcf1e57f3bd3f9bab6a2b529c213a4f2d
AWS-TEAM <agniwebsolution@gmail.com>
2026-07-23 08:16:15 +0530

    Update AnalyticsPlaceholders.jsx

--- src/components/seo/AnalyticsPlaceholders.jsx
+++ src/components/seo/AnalyticsPlaceholders.jsx
@@ -1,16 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { analyticsConfig } from '../../utils/analytics';
 
-function SearchConsoleVerification() {
-  if (!analyticsConfig.searchConsoleVerification) return null;
-  return (
-    <meta
-      name="google-site-verification"
-      content={analyticsConfig.searchConsoleVerification}
-    />
-  );
-}
-
 /**
  * Injects GA4 / GTM / Meta Pixel / Ads placeholders when enabled.
  * Search Console meta tag is always emitted when configured (no cookie consent required).
@@ -20,43 +10,53 @@ export default function AnalyticsPlaceholders({ consent = false }) {
 
   return (
     <Helmet>
-      <SearchConsoleVerification />
+      {analyticsConfig.searchConsoleVerification ? (
+        <meta
+          name="google-site-verification"
+          content={analyticsConfig.searchConsoleVerification}
+        />
+      ) : null}
 
-      {!trackingEnabled ? (
-        !analyticsConfig.searchConsoleVerification ? (
-          <meta name="capilla:analytics" content="placeholders-ready" />
-        ) : null
-      ) : (
-        <>
-          {analyticsConfig.gtmContainerId && analyticsConfig.gtmContainerId !== 'GTM-XXXXXXX' && (
-            <script>{`
+      {!trackingEnabled && !analyticsConfig.searchConsoleVerification ? (
+        <meta name="capilla:analytics" content="placeholders-ready" />
+      ) : null}
+
+      {trackingEnabled &&
+      analyticsConfig.gtmContainerId &&
+      analyticsConfig.gtmContainerId !== 'GTM-XXXXXXX' ? (
+        <script>{`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${analyticsConfig.gtmContainerId}');
         `}</script>
-          )}
+      ) : null}
 
-          {analyticsConfig.ga4MeasurementId &&
-            analyticsConfig.ga4MeasurementId !== 'G-XXXXXXXXXX' && (
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${analyticsConfig.ga4MeasurementId}`}
-              />
-            )}
-          {analyticsConfig.ga4MeasurementId &&
-            analyticsConfig.ga4MeasurementId !== 'G-XXXXXXXXXX' && (
-              <script>{`
+      {trackingEnabled &&
+      analyticsConfig.ga4MeasurementId &&
+      analyticsConfig.ga4MeasurementId !== 'G-XXXXXXXXXX' ? (
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${analyticsConfig.ga4MeasurementId}`}
+        />
+      ) : null}
+
+      {trackingEnabled &&
+      analyticsConfig.ga4MeasurementId &&
+      analyticsConfig.ga4MeasurementId !== 'G-XXXXXXXXXX' ? (
+        <script>{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${analyticsConfig.ga4MeasurementId}');
         `}</script>
-            )}
+      ) : null}
 
-          {analyticsConfig.metaPixelId && analyticsConfig.metaPixelId !== '000000000000000' && (
-            <script>{`
+      {trackingEnabled &&
+      analyticsConfig.metaPixelId &&
+      analyticsConfig.metaPixelId !== '000000000000000' ? (
+        <script>{`
           !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
           n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
@@ -65,9 +65,7 @@ export default function AnalyticsPlaceholders({ consent = false }) {
           fbq('init', '${analyticsConfig.metaPixelId}');
           fbq('track', 'PageView');
         `}</script>
-          )}
-        </>
-      )}
+      ) : null}
     </Helmet>
   );
 }
