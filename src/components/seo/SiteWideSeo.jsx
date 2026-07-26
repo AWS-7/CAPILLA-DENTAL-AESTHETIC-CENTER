bacad41b381a123454b7c82df46efa53497acd03
AWS-TEAM <agniwebsolution@gmail.com>
2026-07-26 19:19:52 +0530

    files upload

--- src/components/seo/SiteWideSeo.jsx
+++ src/components/seo/SiteWideSeo.jsx
@@ -25,13 +25,7 @@ export default function SiteWideSeo() {
         <meta name="geo.position" content={seoDefaults.geo.position} />
         <meta name="ICBM" content={seoDefaults.geo.icbm} />
         <meta name="format-detection" content="telephone=yes" />
-        {import.meta.env.PROD && <link rel="manifest" href="/manifest.webmanifest" />}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Capilla" />
-        <meta name="application-name" content="Capilla Clinic" />
-        <meta name="msapplication-config" content="/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#C8A45D" />
       </Helmet>
 
       <JsonLd data={schemas} />
